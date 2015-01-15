f29b53bfaa960cff7f005f4bbe24b20abc9393ff
Amy Bakameyer <bymak87@gmail.com>
2015-01-14 21:29:02 -0600

    now uses mongo for persistant data

--- simple-todos.js
+++ simple-todos.js
@@ -4,6 +4,8 @@ Tasks = new Mongo.Collection("tasks");
 if(Meteor.isClient){
   // This code only runs on the client
   Template.body.helpers({
-
+    tasks: function () {
+      return Tasks.find({});
+    }
   });
 }
