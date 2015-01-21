4f19b3859038f7a7a7d4955a6d4cc98767ad5ea7
Amy Bakameyer <bymak87@gmail.com>
2015-01-21 13:16:44 -0600

    lesson 8 stores temp ui states in sessions

--- simple-todos.js
+++ simple-todos.js
@@ -23,11 +23,27 @@ if(Meteor.isClient){
 
       //prevent default form submit
       return false;
+    },
+    "change .hide-completed input": function (){
+      Session.set("hideCompleted", event.target.checked);
     }
   });
+
   Template.body.helpers({
-    tasks: function(){
-      return Tasks.find({}, {sort: {createdAt: -1}});
+    tasks: function (){
+      //if hide completed tasks is checked, filter tasks
+      if (Session.get('hideCompleted')){
+        return Tasks.find({checked: {$ne:true}}, {sort: {createdAt: -1}});
+      } else {
+        return Tasks.find({}, {sort: {createdAt: -1}});
+      }
+    },
+    //otherwise return all tasks
+    hideCompleted: function(){
+      return Session.get('hideCompleted');
+    },
+    incompleteCount: function (){
+      return Tasks.find({checked: {$ne: true}}).count();
     }
   });
 
