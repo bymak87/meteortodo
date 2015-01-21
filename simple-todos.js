e7e15fc830d58991e3c6b2eda5201663bd9636b7
Amy Bakameyer <bymak87@gmail.com>
2015-01-21 12:33:28 -0600

    lesson 5

--- simple-todos.js
+++ simple-todos.js
@@ -8,4 +8,21 @@ if(Meteor.isClient){
       return Tasks.find({});
     }
   });
+
+  Template.body.events({
+    "submit .new-task" : function(event){
+      //this function is called when the new task form is submitted
+      var text = event.target.text.value;
+
+      Tasks.insert({
+        text: text,
+        createdAt: new Date()
+      });
+      //clear form
+      event.target.text.value="";
+
+      //prevent default form submit
+      return false;
+    }
+  });
 }
